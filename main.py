f3291da2713275e990ed571b6f6d3d476ce4058b
ChungYi Zhen <nightofwing16@gmail.com>
2020-11-15 17:27:55 +0800

    output include laws version

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ word_file = dict_path / 'oth_words.txt'
 punc_file = dict_path / 'punctuation.pkl'
 split_rule_kw_file = dict_path / 'split_rule_words.txt'
 # output file name
-output_path = root_path / 'output/exclude_rule_ver'
+output_path = root_path / 'output/include_rule_ver'
 # without rule exclusion
 step1_file = output_path / 'data_etl_step1_exclude_law.xlsx'  # output as excel to avoid comma parse error
 step2_file = output_path / 'data_etl_step2.csv'
@@ -25,21 +25,20 @@ w2v_model_file = output_path / 'w2v_var.pkl'
 # output_path / 'data_etl_step2_noPunc.csv'
 
 # # load raw data
-# raw_df = pd.read_excel(root_path / 'data/200801至202008缺失類型(法規分段例).xlsx', sheet_name='法規')
+raw_df = pd.read_excel(root_path / 'data/200801至202008缺失類型(法規分段例).xlsx', sheet_name='法規')
 # # exclude law
 # exclude_rule(raw_df, step1_file, legal_name_file, split_rule_kw_file)
 # raw_df = pd.read_excel(step1_file)
 # # preprocessing
-# data_cleansing(raw_df, output_file=step2_file, text_column=parsed_column)
-# get_punctuation(step2_file, output_file=punc_file, text_column=parsed_column)
-# word_cut(step2_file, step3_file, punc_file, parsed_column, legal_name_file, word_file, ckip_path)
+data_cleansing(raw_df, output_file=step2_file, text_column=parsed_column)
+get_punctuation(step2_file, output_file=punc_file, text_column=parsed_column)
+word_cut(step2_file, step3_file, punc_file, parsed_column, legal_name_file, word_file, ckip_path)
 # model building
-
-# seems na: AttributeError: 'float' object has no attribute 'replace'
 word2vec_model(step3_file, w2v_model_file)
 # recommendation
 starttime = datetime.datetime.now()
 newtext = '開標時有作拒絕往來廠商調查'
+# if word segment replace to jieba maybe exe size would smaller
 result = recommend_law(newtext, w2v_model_file, ckip_path)
 # calculate running time
 endtime = datetime.datetime.now()
